ddac9cea5cd9c6f53e948ef6c3956781c14454ed
Getúlio Werle <getuliowerle10@gmail.com>
2025-02-01 16:50:24 -0400

    main page altered and added favicon

--- app/views/index.php
+++ app/views/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= APP_NAME ?></title>
+    <link rel="shortcut icon" href="assets/img/favicon.svg" type="image/x-icon">
     <!-- bootstrap css -->
     <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
 </head>
@@ -15,19 +16,22 @@
         <div class="row justify-content-center">
             <div class="col-12 col-md-8 col-lg-6 shadow-lg bg-light-emphasis p-5 rounded-5">
                 <h1 class="text-center mb-5"><?= APP_NAME ?></h1>
-                <form action="?ct=main&mt=make_calendar" method="POST">
+                <form action="?ct=main&mt=make_calendar" method="POST" class="mb-5">
                     <div class="mb-4">
                         <label for="month_and_year" class="form-label">Month</label>
                         <input type="month" name="month_and_year" class="form-control" required>
                     </div>
                     <div id="events"></div>
-                    <div class="mb-5 text-center">
+                    <div class="my-5 text-center">
                         <button type="button" id="btn_add_event" class="btn btn-success p-3 w-50">+ Add Event</button>
                     </div>
                     <div class="text-center">
                         <button type="submit" class="btn btn-primary p-3 w-50">Generate</button>
                     </div>
                 </form>
+                <div class="text-center opacity-50">
+                    <p>Feito por Getúlio Werle</p>
+                </div>
             </div>
         </div>
     </div>
